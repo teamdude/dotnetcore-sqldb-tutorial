5fba1265a1dc0f65c65659dee5863bdfdd95287b
Peter Odufona <teamdude@users.noreply.github.com>
2018-11-17 01:16:37 +0000

    Update Startup.cs
    Updated Configuration.GetConnectionString("DefaultConnection") to Configuration.GetConnectionString("Default")

--- Startup.cs
+++ Startup.cs
@@ -30,7 +30,7 @@ namespace DotNetCoreSqlDb
         // Use SQL Database if in Azure, otherwise, use SQLite
         if(Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
             services.AddDbContext<MyDatabaseContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+                    options.UseSqlServer(Configuration.GetConnectionString("Default")));
         else
             services.AddDbContext<MyDatabaseContext>(options =>
                     options.UseSqlite("Data Source=localdatabase.db"));
